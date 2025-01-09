15c152968d3f91f9a54867e1b0462abd24977c10
Prasanth_Sagar <prasanth.sagar100@gmail.com>
2025-01-10 02:57:49 +0530

    Fetching optimized images by modifying image url

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { IGame } from "@/hooks/useFetchGames"
 import { Card, CardBody, Heading, Image, HStack } from "@chakra-ui/react"
 import PlatformIconList from "./PlatformIconList"
 import CriticScore from "./CriticScore"
+import { getCroppedImageUrl } from "@/services/utils"
 
 interface Props {
   game: IGame
@@ -11,7 +12,7 @@ function GameCard({ game }: Props) {
   return (
     <>
       <Card borderRadius={10} overflow="hidden">
-        <Image src={game.background_image}></Image>
+        <Image src={getCroppedImageUrl(game.background_image)}></Image>
         <CardBody>
           <Heading fontSize="2xl">{game.name}</Heading>
           <HStack justifyContent="space-between">

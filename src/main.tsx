c3fde094e7bbd70f9d6802306e0a5e9e28987613
Prasanth_Sagar <prasanth.sagar100@gmail.com>
2025-01-09 18:29:17 +0530

    Base structure created and Chakra issues fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider as ChakraProvider } from '@chakra-ui/react/provider';
+import { ChakraProvider } from '@chakra-ui/react';
 import './index.css';
 import App from './App.tsx';
 

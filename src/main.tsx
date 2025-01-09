ed688a4299e6b5feab9cdccb359a179875f75e33
Prasanth_Sagar <prasanth.sagar100@gmail.com>
2025-01-09 17:01:55 +0530

    Added configuration to support Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider as ChakraProvider } from '@chakra-ui/react/provider';
+import './index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
-)
+);

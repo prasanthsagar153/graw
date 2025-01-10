6f28f37c68abfbabf67727129a6746539d1bd777
Prasanth_Sagar <prasanth.sagar100@gmail.com>
2025-01-11 03:14:18 +0530

    Created Genre List component

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,27 @@
 import useFetchGenres from "@/hooks/useFetchGenres";
+import { getCroppedImageUrl } from "@/services/utils";
+import { HStack, List, ListItem, Image, Spinner, Button } from "@chakra-ui/react";
 
 function GenreList() {
   const { data: genres, loading, error } = useFetchGenres();
+  if(loading) return <Spinner />
+  if(error) return null;
   return (
     <>
-      { JSON.stringify(genres) }
+      <List>
+        {
+          genres.map(g => {
+            return (
+              <ListItem key={g.id} paddingY="5px">
+                <HStack>
+                  <Image boxSize="32px" borderRadius={8} src={getCroppedImageUrl(g.image_background)} />
+                  <Button fontSize="lg" variant="link">{g.name}</Button>
+                </HStack>
+              </ListItem>
+            )
+          })
+        }
+      </List>
     </>
   )
 }
--- src/hooks/useFetchGenres.ts
+++ src/hooks/useFetchGenres.ts
@@ -3,6 +3,7 @@ import useFetchData from "./useFetchData";
 export interface IGenre {
   id: number;
   name: string;
+  image_background: string;
 }
 
 export default function useFetchGenres () {
